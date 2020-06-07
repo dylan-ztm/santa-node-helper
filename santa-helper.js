7f6cf1f6ab3574bf7593e9bd4e69fafa86d30f30
Dylan Cross <dylan@dylancross.us>
2020-06-07 11:40:26 -0700

    Updated script to include Part 2 solution.

--- santa-helper.js
+++ santa-helper.js
@@ -30,13 +30,31 @@ const computeLastFloor = (fileContent) => {
     return currentFloor;
 } //end computeLastFloor
 
+const computeBasementPosition = (fileContent) => {
+    let instructions = fileContent.toString();
+    let currentFloor = 0;
+    let position     = 0;
+
+    for (let i = 0; i < instructions.length; i++) {
+        let currentCharacter = instructions.charAt(i);
+        currentFloor = findNextFloor(currentFloor, currentCharacter);
+        if (currentFloor === -1) {
+            position = i + 1;
+            return position;
+        } //end if
+    } //end for loop
+    return 0;
+} //end computeBasementPosition
+
 // Read data from INSTRUCTION_FILE
 fs.readFile(INSTRUCTION_FILE, (error, fileContent) => {
     console.time('Script execution time: ');
     try {
-            let lastFloor;
-            lastFloor = computeLastFloor(fileContent);
+            let lastFloor        = computeLastFloor(fileContent);
+            let basementPosition = computeBasementPosition(fileContent);
             console.log (`The instructions take Santa to floor number: ${lastFloor}`);
+            (basementPosition > 0) ? console.log(`Santa first entered the basement floor at position: ${basementPosition}`)
+                                   : console.log('Santa did not visit the basement floor.');
         }
     catch (error) {
         console.error(`Whoops! We found the following error: ${error}`);
